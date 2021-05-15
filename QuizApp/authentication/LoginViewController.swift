755f980fb795b141253bb08edf4a61917f88b936
bkouki <51118623+bkouki@users.noreply.github.com>
2021-05-15 14:38:40 +0100

    extract input from fields

--- QuizApp/authentication/LoginViewController.swift
+++ QuizApp/authentication/LoginViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func onLoginClicked(_ sender: UIButton) {
+        let email: String = emailInput.text!
+        let password: String = passwordInput.text!
+        
+        print(email)
+        print(password)
+    }
+    
+    @IBAction func passwordForgotten(_ sender: UIButton) {
+        
     }
 }
--- QuizApp/inscription/SignUpViewController.swift
+++ QuizApp/inscription/SignUpViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var emailInput: UITextField!
+    @IBOutlet weak var nameInput: UITextField!
+    @IBOutlet weak var usernameInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,15 +26,16 @@ class SignUpViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func onSignupClicked(_ sender: UIButton) {
+        let email = self.emailInput.text
+        let name = self.nameInput.text
+        let username = self.usernameInput.text
+        let password = self.passwordInput.text
+        
+        print(email)
+        print(name)
+        print(username)
+        print(password)
+        
     }
-    */
-
 }
